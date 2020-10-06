9015f8cc2052edb2fcdfcf28795838f2122dd807
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-06 15:38:30 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Cloutier, Vincent on 2020-10-06.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
