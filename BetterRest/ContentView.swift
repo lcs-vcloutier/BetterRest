21661dea78ade2f62c3821a5266324ed0203bb7d
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-06 15:40:56 -0400

    stepper test

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,9 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sleepAmount = 8.0
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+            Text("\(sleepAmount, specifier: "%.2f") hours")
+
+        }
+
     }
 }
 
