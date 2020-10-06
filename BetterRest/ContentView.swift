d4778517b430f4d14efbb9e27c537822bff52a21
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-06 15:44:22 -0400

    timeonly

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,19 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date()
 
     var body: some View {
-        Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
-            Text("\(sleepAmount, specifier: "%.2f") hours")
-
-        }
+        DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
 
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }
